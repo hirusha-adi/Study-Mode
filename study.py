40b0cefdd8588721847a1d01324c2b9cd6cce93d
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-04-02 10:38:09 +0530

    Update study.py

--- study.py
+++ study.py
@@ -25,7 +25,6 @@ def parse_commands(content: str):
 
 
 def run():
-    cwd = os.getcwd()
     for name in os.listdir(os.getcwd()):
         if name.endswith(".study"):
             with open(os.path.join(os.getcwd(), name), 'r', encoding='utf-8') as file:
